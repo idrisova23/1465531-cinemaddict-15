3711d937d394f6139a04399cb2a931159ec43e51
bublikk <yuliya.vlasovaa@gmail.com>
2021-08-14 18:52:50 +0300

    Добавит структуру данных комментариев

--- src/view/comments.js
+++ src/view/comments.js
@@ -1,22 +1,35 @@
-export const createCommentsTemplate = () => (
-  `<div class="film-details__bottom-container">
+const createCommentsList = (comments) => {
+  let commentsList = '';
+
+  for (let i = 0; i < comments.length; i++) {
+    commentsList += `<li class="film-details__comment">
+    <span class="film-details__comment-emoji">
+      <img src="./images/emoji/${comments[i].emotion}.png" width="55" height="55" alt="emoji-${comments[i].emotion}">
+    </span>
+    <div>
+      <p class="film-details__comment-text">${comments[i].comment}</p>
+      <p class="film-details__comment-info">
+        <span class="film-details__comment-author">${comments[i].author}</span>
+        <span class="film-details__comment-day">${comments[i].date}</span>
+        <button class="film-details__comment-delete">Delete</button>
+      </p>
+    </div>
+  </li>`;
+  }
+
+  return commentsList;
+};
+
+export const createCommentsTemplate = (film) => {
+  const {comments} = film;
+  const commentsList = createCommentsList(comments);
+
+  return `<div class="film-details__bottom-container">
     <section class="film-details__comments-wrap">
-      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">4</span></h3>
+      <h3 class="film-details__comments-title">Comments <span class="film-details__comments-count">${comments.length}</span></h3>
 
       <ul class="film-details__comments-list">
-        <li class="film-details__comment">
-          <span class="film-details__comment-emoji">
-            <img src="./images/emoji/smile.png" width="55" height="55" alt="emoji-smile">
-          </span>
-          <div>
-            <p class="film-details__comment-text">Interesting setting and a good cast</p>
-            <p class="film-details__comment-info">
-              <span class="film-details__comment-author">Tim Macoveev</span>
-              <span class="film-details__comment-day">2019/12/31 23:59</span>
-              <button class="film-details__comment-delete">Delete</button>
-            </p>
-          </div>
-        </li>
+        ${commentsList}
       </ul>
 
       <div class="film-details__new-comment">
@@ -34,5 +47,5 @@ export const createCommentsTemplate = () => (
         </div>
       </div>
     </section>
-  </div>`
-);
+  </div>`;
+};
