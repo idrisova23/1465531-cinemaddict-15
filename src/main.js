bd2953e5f8206432035d9b1a5bd8ee992dbea175
bublikk <yuliya.vlasovaa@gmail.com>
2021-08-26 14:38:52 +0300

    Отрефакторит модуль с утилитами

--- src/main.js
+++ src/main.js
@@ -11,7 +11,7 @@ import FilmDetailsView from './view/film-details.js';
 import CommentListView from './view/comment-list.js';
 import NewCommentFormView from './view/new-comment-form.js';
 import {generateFilm, generateFilter, generateFooterStats} from './mock/mock.js';
-import {render, RenderPosition} from './utils.js';
+import {render, RenderPosition, remove} from './utils/render.js';
 
 export const FILM_COUNT = 18;
 const FILM_COUNT_PER_STEP = 5;
@@ -24,10 +24,10 @@ const siteBodyElement = document.querySelector('body');
 const siteHeaderElement = document.querySelector('.header');
 const siteMainElement = document.querySelector('.main');
 
-render(siteHeaderElement, new ProfileView(films).getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilterMenuView(filters).getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new SortView().getElement(), RenderPosition.BEFOREEND);
-render(siteMainElement, new FilmListView().getElement(), RenderPosition.BEFOREEND);
+render(siteHeaderElement, new ProfileView(films), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilterMenuView(filters), RenderPosition.BEFOREEND);
+render(siteMainElement, new SortView(), RenderPosition.BEFOREEND);
+render(siteMainElement, new FilmListView(), RenderPosition.BEFOREEND);
 
 const filmsList = document.querySelector('.films-list');
 const filmsListContainer = filmsList.querySelector('.films-list__container');
@@ -43,6 +43,7 @@ const renderFilm = (filmListElement, film) => {
     if (evt.key === 'Escape') {
       siteBodyElement.removeChild(popupComponent.getElement());
       siteBodyElement.classList.remove('hide-overflow');
+
       document.removeEventListener('keydown', handleKeydown);
     }
   };
@@ -50,6 +51,7 @@ const renderFilm = (filmListElement, film) => {
   filmDetailsComponent.setCloseClickHandler(() => {
     siteBodyElement.removeChild(popupComponent.getElement());
     siteBodyElement.classList.remove('hide-overflow');
+
     document.removeEventListener('keydown', handleKeydown);
   });
 
@@ -68,11 +70,11 @@ const renderFilm = (filmListElement, film) => {
   filmComponent.setFilmClickHandler('.film-card__title', fn);
   filmComponent.setFilmClickHandler('.film-card__comments', fn);
 
-  render(filmListElement, filmComponent.getElement(), RenderPosition.BEFOREEND);
+  render(filmListElement, filmComponent, RenderPosition.BEFOREEND);
 };
 
 if (films.length === 0) {
-  render(siteMainElement, new ListEmptyView().getElement(), RenderPosition.BEFOREEND);
+  render(siteMainElement, new ListEmptyView(), RenderPosition.BEFOREEND);
 } else {
   for (let i = 0; i < Math.min(films.length, FILM_COUNT_PER_STEP); i++) {
     renderFilm(filmsListContainer, films[i]);
@@ -84,7 +86,7 @@ if (films.length > FILM_COUNT_PER_STEP) {
 
   const showMoreButtonComponent = new ShowMoreButtonView();
 
-  render(filmsList, showMoreButtonComponent.getElement(), RenderPosition.BEFOREEND);
+  render(filmsList, showMoreButtonComponent, RenderPosition.BEFOREEND);
 
   showMoreButtonComponent.setClickHandler(() => {
     films
@@ -94,12 +96,11 @@ if (films.length > FILM_COUNT_PER_STEP) {
     renderedFilmCount += FILM_COUNT_PER_STEP;
 
     if (renderedFilmCount >= films.length) {
-      showMoreButtonComponent.getElement().remove();
-      showMoreButtonComponent.removeElement();
+      remove(showMoreButtonComponent);
     }
   });
 }
 
 const footerStatistics = document.querySelector('.footer__statistics');
 
-render(footerStatistics, new FooterStatsView(statistics).getElement(), RenderPosition.BEFOREEND);
+render(footerStatistics, new FooterStatsView(statistics), RenderPosition.BEFOREEND);
