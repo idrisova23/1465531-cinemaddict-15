7e4263597d199f46c396c7981eb084f588c50615
bublikk <yuliya.vlasovaa@gmail.com>
2021-09-02 18:41:31 +0300

    Поправит шаблон сортировки

--- src/view/sort.js
+++ src/view/sort.js
@@ -1,15 +1,26 @@
 import AbstractView from './abstract.js';
 
-const createSortTemplate = () => (
+export const SortType = {
+  DEFAULT: 'default',
+  DATE_DOWN: 'date-down',
+  RATING_DOWN: 'rating-down',
+};
+
+const createSortTemplate = (currentSortType) => (
   `<ul class="sort">
-    <li><a href="#" class="sort__button sort__button--active">Sort by default</a></li>
-    <li><a href="#" class="sort__button">Sort by date</a></li>
-    <li><a href="#" class="sort__button">Sort by rating</a></li>
+    <li><a href="#" class="sort__button ${currentSortType === SortType.DEFAULT && 'sort__button--active'}" data-sort-type="${SortType.DEFAULT}">Sort by default</a></li>
+    <li><a href="#" class="sort__button ${currentSortType === SortType.DATE_DOWN && 'sort__button--active'}" data-sort-type="${SortType.DATE_DOWN}">Sort by date</a></li>
+    <li><a href="#" class="sort__button ${currentSortType === SortType.RATING_DOWN && 'sort__button--active'}" data-sort-type="${SortType.RATING_DOWN}">Sort by rating</a></li>
   </ul>`
 );
 
 export default class Sort extends AbstractView {
+  constructor(currentSortType) {
+    super();
+    this._currentSortType = currentSortType;
+  }
+
   getTemplate() {
-    return createSortTemplate();
+    return createSortTemplate(this._currentSortType);
   }
 }
