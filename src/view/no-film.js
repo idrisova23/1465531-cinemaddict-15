b348d2ecd30804e2d42984853bcd9de3e42e4668
bublikk <yuliya.vlasovaa@gmail.com>
2021-09-14 00:19:57 +0300

    Поправит компонент-заглушки

--- src/view/no-film.js
+++ src/view/no-film.js
@@ -13,8 +13,7 @@ const createNoFilmTemplate = (filterType) => {
 
   return `<section class="films">
     <section class="films-list">
-      <h2 class="films-list__title">There are no movies in our database</h2>
-      ${noFilmTextValue}
+      <h2 class="films-list__title">${noFilmTextValue}</h2>
     </section>
   </section>`;
 };
